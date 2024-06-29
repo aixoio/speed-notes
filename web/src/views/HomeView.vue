5f722c741591ce030f2ec7c509470fae3d57c714
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 22:46:30 +1000

    Update master.scss and HomeView.vue files

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -5,8 +5,8 @@
         <h1 class="text-3xl">Speed notes</h1>
       </div>
       <div class="flex gap-2">
-        <button class="p-2 m-3 border border-gray-950 rounded-2xl bg-gray-100 text-xl cursor-pointer hover:bg-transparent shadow hover:shadow-lg">Signup</button>
-        <button class="p-2 m-3 border border-gray-950 rounded-2xl bg-gray-100 text-xl cursor-pointer hover:bg-transparent shadow hover:shadow-lg">Login</button>
+        <button class="btn">Signup</button>
+        <button class="btn">Login</button>
       </div>
     </div>
   </div>
