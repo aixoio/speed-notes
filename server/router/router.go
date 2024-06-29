5df34bb3f064bd02691ed353d99ab76e71780398
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 19:04:37 +1000

    Update router logic in server/router/router.go

--- server/router/router.go
+++ server/router/router.go
@@ -7,12 +7,15 @@ import (
 	"github.com/aixoio/speed-notes/server/env"
 	"github.com/aixoio/speed-notes/server/router/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func StartRouter(cfg env.Config, db *sql.DB) {
 	handlers.DB = db
 
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Post("/signup", handlers.SignupHandler)
 
