02923550575dd794cf5230d21b200a748c7ef46c
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 22:45:49 +1000

    Update master.scss and HomeView.vue files

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <div class="flex justify-between border-b border-b-gray-950">
+      <div class="flex items-center align-middle">
+        <h1 class="text-3xl">Speed notes</h1>
+      </div>
+      <div class="flex gap-2">
+        <button class="p-2 m-3 border border-gray-950 rounded-2xl bg-gray-100 text-xl cursor-pointer hover:bg-transparent shadow hover:shadow-lg">Signup</button>
+        <button class="p-2 m-3 border border-gray-950 rounded-2xl bg-gray-100 text-xl cursor-pointer hover:bg-transparent shadow hover:shadow-lg">Login</button>
+      </div>
+    </div>
   </div>
 </template>
