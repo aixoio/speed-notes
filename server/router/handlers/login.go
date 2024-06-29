3252469e4398f7d6e322d5d483e8295aa94757bd
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 19:32:14 +1000

    Updated login handler logic in server/router/handlers/login.go

--- server/router/handlers/login.go
+++ server/router/handlers/login.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/aixoio/speed-notes/server/tools"
+	"github.com/gofiber/fiber/v2"
+)
 
 type login_request struct {
 	Username string `json:"username"`
@@ -9,4 +12,33 @@ type login_request struct {
 
 func LoginHandler(c *fiber.Ctx) error {
 	c.Accepts("application/json")
+
+	var dat login_request
+
+	if err := c.BodyParser(&dat); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse request body"})
+	}
+
+	if !tools.IsUsernameValid(dat.Username) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "The username is invalid"})
+	}
+
+	exists, err := tools.UserExists(dat.Username, DB)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot check username"})
+	}
+
+	if !exists {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+	}
+
+	user, err := tools.UserFromUsername(dat.Username, DB)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "PostgreSQL cannot find user"})
+	}
+
+	if !tools.VerifyPassword(user.Password_hash, dat.Password) {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Bad Password"})
+	}
+
 }
