e52e37b97f511c75819b173049262f07eabda4a9
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 21:26:59 +1000

    Updated note.go file in the server/data directory.

--- server/data/note.go
+++ server/data/note.go
@@ -1,8 +1,8 @@
 package data
 
 type Note struct {
-	Id       uint32
-	User_id  uint32
-	Title    string
-	Contents string
+	Id       uint32 `json:"id"`
+	User_id  uint32 `json:"user_id"`
+	Title    string `json:"title"`
+	Contents string `json:"contents"`
 }
