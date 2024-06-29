b4e3ac1078e3542cac4948387fff8f8bc69d78c6
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 18:06:56 +1000

    Update env.go file on the server side

--- server/env/env.go
+++ server/env/env.go
@@ -1,1 +1,27 @@
 package env
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadConfig() Config {
+	cfg, done := loadFromSystem()
+	if !done {
+		godotenv.Load()
+		return Config{
+			Port: os.Getenv("PORT"),
+		}
+	}
+	return cfg
+}
+
+func loadFromSystem() (Config, bool) {
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		return Config{}, false
+	}
+
+	return Config{Port: port}, true
+}
