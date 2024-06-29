a26b3fec7e687d6eb0e91dab11bbc6290c0b6dfc
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 18:18:02 +1000

    Update PostgreSQL configuration file.

--- server/db/postgresql/postgresql.go
+++ server/db/postgresql/postgresql.go
@@ -1,1 +1,20 @@
 package postgresql
+
+import (
+	"database/sql"
+
+	"github.com/aixoio/speed-notes/server/env"
+)
+
+func Connect(cfg env.Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.Postgresql_url)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
