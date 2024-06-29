e8bf87e5dd02b516c771e11b85513c67f1d7df8c
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 20:19:07 +1000

    Updated notes handler in router.

--- server/router/handlers/notes.go
+++ server/router/handlers/notes.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/aixoio/speed-notes/server/tools"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -13,4 +14,10 @@ func NotesHandlers(c *fiber.Ctx) error {
 
 	uid := uint32(claims["id"].(float64))
 
+	notes, err := tools.AllNotesByUserID(uid, DB)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "PostgreSQL cannot find notes"})
+	}
+
+	return c.JSON(notes)
 }
