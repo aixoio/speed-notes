101e3b55c2b61d34dd9ad46a780ee8a0bbb7b893
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 23:17:25 +1000

    Updated SignupView.vue with modifications

--- web/src/views/SignupView.vue
+++ web/src/views/SignupView.vue
@@ -8,6 +8,9 @@
             <div class="grid grid-cols-1">
                 <span>Username</span>
                 <input type="text" placeholder="Username" class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none" maxlength="255">
+                <span>Password</span>
+                <input type="password" placeholder="Password" class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none">
+                <button class="btn">Signup</button>
             </div>
         </div>
     </div>
