8cbfc99523c6038904a037a337cab2d490b8367c
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 18:23:47 +1000

    Update users.go file in server/tools directory

--- server/tools/users.go
+++ server/tools/users.go
@@ -1,7 +1,14 @@
 package tools
 
-import "regexp"
+import (
+	"database/sql"
+	"regexp"
+)
 
 func IsUsernameValid(un string) bool {
 	return !regexp.MustCompile("[^A-z0-9]+").MatchString(un)
+}
+
+func UserExists(username string, db *sql.DB) (bool, error) {
+
 }
