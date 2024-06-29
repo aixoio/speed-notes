15e0fee3b673a1826a99ecbdd4e55b47bef11ac8
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 23:15:37 +1000

    Updated SignupView.vue with modifications

--- web/src/views/SignupView.vue
+++ web/src/views/SignupView.vue
@@ -4,6 +4,12 @@
         <div class="flex justify-center m-4">
             <h1 class="text-4xl font-mono">Signup</h1>
         </div>
+        <div class="flex justify-center">
+            <div class="grid grid-cols-1">
+                <span>Username</span>
+                <input type="text" placeholder="Username" class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none" maxlength="255">
+            </div>
+        </div>
     </div>
 </template>
 
