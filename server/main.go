c929e6c5280d75dfdb98cf90674ecc71bebad774
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-29 18:18:43 +1000

    Update server/main.go

--- server/main.go
+++ server/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/aixoio/speed-notes/server/db/postgresql"
 	"github.com/aixoio/speed-notes/server/env"
 	"github.com/aixoio/speed-notes/server/router"
 )
 
 func main() {
 	cfg := env.LoadConfig()
+
+	db, err := postgresql.Connect(cfg)
+	if err != nil {
+		panic(err)
+	}
 
 	router.StartRouter(cfg)
 }
