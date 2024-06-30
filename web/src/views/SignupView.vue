5527f4fafd5adf0b8a3a3781971af76b8f1333f2
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 10:11:45 +1000

    Update package.json, pnpm-lock.yaml, and SignupView.vue files.

--- web/src/views/SignupView.vue
+++ web/src/views/SignupView.vue
@@ -7,10 +7,14 @@
         <div class="flex justify-center">
             <div class="grid grid-cols-1">
                 <span>Username</span>
-                <input type="text" placeholder="Username" class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none" maxlength="255" :value="username">
+                <input type="text" placeholder="Username"
+                    class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none" maxlength="255"
+                    v-model="username">
                 <span>Password</span>
-                <input type="password" placeholder="Password" class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none" :value="password">
-                <button class="btn" @click="signup">Signup</button>
+                <input type="password" placeholder="Password"
+                    class="border border-gray-950 rounded-lg m-4 bg-gray-50 p-2 outline-none" v-model="password">
+                <span class="bg-red-200 p-2 m-2 border border-red-700 rounded-lg text-red-950" v-show="error != ''">{{ error }}</span>
+                <button class="btn" @click.prevent="signup">Signup</button>
             </div>
         </div>
     </div>
@@ -19,12 +23,18 @@
 <script lang="ts" setup>
 import NavBar from '@/components/NavBar.vue';
 import { ref } from 'vue';
+import { isEmpty } from "lodash"
+
 
 let username = ref("");
 let password = ref("");
+let error = ref("");
 
 function signup() {
-    
+    if (isEmpty(username.value.trim()) || isEmpty(password.value.trim())) {
+        error.value = "You must enter a username and password"
+        return
+    }
 }
 
 </script>
