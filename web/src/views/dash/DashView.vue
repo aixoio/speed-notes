17486cf982a5905917e08d64fb54d02b99eb4ed7
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 11:46:17 +1000

    Updated DashView.vue with modifications

--- web/src/views/dash/DashView.vue
+++ web/src/views/dash/DashView.vue
@@ -6,15 +6,32 @@
             <h1 class="text-2xl">Notes</h1>
         </div>
         <div class="grid grid-cols-4 gap-4">
-            <div class="border border-gray-950 p-2 rounded bg-slate-100 shadow cursor-pointer">
-                <span class="font-extrabold text-lg">[NOTE TITLE]</span>
-                <p>NOTE DATA</p>
+            <div v-for="note in notesStore.notes">
+                <div class="border border-gray-950 p-2 rounded bg-slate-100 shadow cursor-pointer">
+                    <span class="font-extrabold text-lg">{{ note.title }}</span>
+                    <p>{{ note.contents }}</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { Notes } from '@/assets/ts/tools/notes';
 import DashNavBar from '@/components/DashNavBar.vue';
+import router from '@/router';
+import { useNotesStore } from '@/stores/notesstore';
+import { useUserStore } from '@/stores/userstore';
+import { onMounted } from 'vue';
+
+const notesStore = useNotesStore()
+const userStore = useUserStore()
+
+onMounted(async () => {
+    if (userStore.jwt == null) {
+        router.push({ name: "login" })
+    }
+    notesStore.setNotes(await Notes(userStore.jwt as string))
+})
 
 </script>
