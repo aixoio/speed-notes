d92dd4d258452f453eeb3a79d4eb18168f540edd
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 10:33:30 +1000

    Updated router configuration in web/src/router/index.ts

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -13,6 +13,11 @@ const router = createRouter({
       name: 'signup',
       component: () => import("../views/SignupView.vue"),
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import("../views/LoginView.vue"),
+    },
   ]
 })
 
