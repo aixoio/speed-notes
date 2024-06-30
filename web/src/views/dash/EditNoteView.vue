be0dd6911fb46acd5688acb19a26e28635d07c3a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 14:43:14 +1000

    Added EditNoteView.vue file in web/src/views/dash directory.

---
+++ web/src/views/dash/EditNoteView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <DashNavBar></DashNavBar>
+        <div class="m-4">
+            <h1 class="text-2xl">Editing note - [NOTE TITLE]</h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import DashNavBar from '@/components/DashNavBar.vue';
+
+</script>
