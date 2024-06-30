de6f7ac96d7c52d32f389f2826e1afe608bd6d84
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 10:37:56 +1000

    Updated NavBar component with modifications.

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="flex justify-between border-b border-b-gray-950">
-      <div class="flex items-center align-middle">
-        <h1 class="text-3xl cursor-pointer" @click="home">Speed notes</h1>
-      </div>
-      <div class="flex gap-2">
-        <button class="btn" @click="signup">Signup</button>
-        <button class="btn">Login</button>
-      </div>
+  <div class="flex justify-between border-b border-b-gray-950">
+    <div class="flex items-center align-middle">
+      <h1 class="text-3xl cursor-pointer" @click="home">Speed notes</h1>
     </div>
+    <div class="flex gap-2">
+      <button class="btn" @click="signup">Signup</button>
+      <button class="btn" @click="login">Login</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -19,6 +19,10 @@ function home() {
 
 function signup() {
   router.push({ name: "signup" })
+}
+
+function login() {
+  router.push({ name: "login" })
 }
 
 </script>
