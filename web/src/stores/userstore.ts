3f30c04e5e52388bd8c7be34f69c501c04b868b1
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-30 10:55:11 +1000

    Updated user store logic in web/src/stores/userstore.ts.

--- web/src/stores/userstore.ts
+++ web/src/stores/userstore.ts
@@ -1,5 +1,22 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export const useUserStore = defineStore("user", () => {
-    
+    const jwt = ref<string | null>(localStorage.getItem("speed_notes_jwt"))
+
+    function setJwt(jwt_str: string) {
+        jwt.value = jwt_str
+        localStorage.setItem("speed_notes_jwt", jwt_str)
+    }
+
+    function logout() {
+        jwt.value = null
+        localStorage.removeItem("speed_notes_jwt")
+    }
+
+    return {
+        jwt,
+        setJwt,
+        logout,
+    }
 })
